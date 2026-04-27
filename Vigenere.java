871a3850b729782602ce890c1d92ea384db02c82
tmigisha <trustmigisha@u.boisestate.edu>
2026-04-27 13:19:59 -0600

    Updated Vigenere.java

--- Vigenere.java
+++ Vigenere.java
@@ -4,16 +4,20 @@ import static java.lang.Character.toUpperCase;
 
 public class Vigenere {
     public static char[] letters = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-            'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W','X', 'Y', 'Z'};
+                                    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W','X', 'Y', 'Z'};
 
     private static char[] keyArray;
+    public static String encrypted;
+    public static String decrypted;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String key;
         String plaintext;
 
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println("Welcome to the Vigenere cryptography!");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println("Please enter your plaintext:");
         plaintext = input.nextLine().toUpperCase().replaceAll(" ", "");
 
@@ -32,48 +36,49 @@ public class Vigenere {
         System.out.println();
         System.out.println("Encryption function: E = (Pi + Ki) % 26");
         System.out.println("Plaintext: " + plaintext);
-        System.out.println("Encrypted message: " + encryptionFunction(plaintext));
-        String encrypted = encryptionFunction(plaintext);
+        System.out.println("------------------------------------------------------");
+        encryptionFunction(plaintext);
+        System.out.println("Encrypted message: " + encrypted);
+
 
         System.out.println();
         System.out.println();
         System.out.println("Decryption function: D = (Ci - Ki) % 26");
         System.out.println("Ciphertext: " + encrypted);
-        System.out.println("Decrypted message: " + decryptionFunction(encrypted));
+        System.out.println("------------------------------------------------------");
+        decryptionFunction(encrypted);
+        System.out.println("Decrypted message: " + decrypted);
     }
 
 
-    private static String encryptionFunction(String plaintext) {
+    private static void encryptionFunction(String plaintext) {
         StringBuilder message = new StringBuilder();
-
 
         for (int i = 0; i < plaintext.length(); i++) {
             int index;
 
-//            System.out.println("Pi: " + getLetterIndex(plaintext.charAt(i)));
-//            System.out.println("Ki: " + getLetterIndex(keyArray[i]));
-
+            System.out.println("Pi: " + getLetterIndex(plaintext.charAt(i)));
+            System.out.println("Ki: " + getLetterIndex(keyArray[i]));
 
             index = (getLetterIndex(plaintext.charAt(i)) + getLetterIndex(keyArray[i])) % letters.length;
-//            System.out.println("Index: " + index);
-//            System.out.println();
+            System.out.println("Letter Index: (" + getLetterIndex(plaintext.charAt(i)) + " + "
+                    +  getLetterIndex(keyArray[i]) + ") % 26 = " + index);
+            System.out.println();
 
             message.append(letters[index]);
         }
 
-        return message.toString();
-
+        encrypted = message.toString();
     }
 
-    private static String decryptionFunction(String ciphertext) {
+    private static void decryptionFunction(String ciphertext) {
         StringBuilder message = new StringBuilder();
-
 
         for (int i = 0; i < ciphertext.length(); i++) {
             int index;
 
-//            System.out.println("Pi: " + getLetterIndex(ciphertext.charAt(i)));
-//            System.out.println("Ki: " + getLetterIndex(keyArray[i]));
+            System.out.println("Ci: " + getLetterIndex(ciphertext.charAt(i)));
+            System.out.println("Ki: " + getLetterIndex(keyArray[i]));
 
             int firstPart = getLetterIndex(ciphertext.charAt(i)) - getLetterIndex(keyArray[i]);
             if (firstPart < 0) {
@@ -82,13 +87,14 @@ public class Vigenere {
                 index = firstPart % letters.length;
             }
 
-//            System.out.println("Index: " + index);
-//            System.out.println();
+            System.out.println("Letter Index: (" + getLetterIndex(ciphertext.charAt(i)) + " - "
+                    +  getLetterIndex(keyArray[i]) + ") % 26 = " + index);
+            System.out.println();
 
             message.append(letters[index]);
         }
 
-        return message.toString();
+        decrypted = message.toString();
     }
 
 
