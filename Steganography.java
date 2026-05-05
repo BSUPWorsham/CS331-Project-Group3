268f499a41734c137c6d691f9089caea817067cd
brad <bradleeborgholth@u.boisestate.edu>
2026-05-04 18:12:33 -0600

    embed partial

--- Steganography.java
+++ Steganography.java
@@ -5,6 +5,47 @@ import javax.imageio.ImageIO;
 
 public class Steganography {
 
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            printUsage();
+            return;
+        }
+
+        String command = args[0].toLowerCase();
+
+        try {
+            if (command.equals("embed")) {
+                if (args.length != 4) {
+                    printUsage();
+                    return;
+                }
+                String inputImagePath = args[1];
+                String message = args[2];
+                String outputImagePath = args[3];
+                embedMessage(inputImagePath, message, outputImagePath);
+                System.out.println("Message embedded successfully into: " + outputImagePath);
+            } else if (command.equals("extract")) {
+                if (args.length != 2) {
+                    printUsage();
+                    return;
+                }
+                String imagePath = args[1];
+                String message = extractMessage(imagePath);
+                System.out.println("Extracted message: " + message);
+            } else {
+                printUsage();
+            }
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    private static void printUsage() {
+        System.out.println("Usage:");
+        System.out.println("  java Steganography embed <inputImage> <message> <outputImage>");
+        System.out.println("  java Steganography extract <inputImage>");
+    }
+
     public static void embedMessage(String inputImagePath, String message, String outputImagePath) throws IOException {
         BufferedImage image = ImageIO.read(new File(inputImagePath));
         if (image == null) {
@@ -26,7 +67,7 @@ public class Steganography {
         System.arraycopy(lengthBytes, 0, data, 0, 4);
         System.arraycopy(messageBytes, 0, data, 4, messageLength);
 
-        // Embed bits
+        // Embed bits, 3 bits per pixel in RGB order
         int width = image.getWidth();
         int height = image.getHeight();
         int dataIndex = 0;
@@ -37,30 +78,30 @@ public class Steganography {
                 if (dataIndex >= data.length) break outer;
 
                 int rgb = image.getRGB(x, y);
-                int component = bitIndex % 3; // 0=R, 1=G, 2=B
-                int bit = (data[dataIndex] >> (7 - bitIndex % 8)) & 1;
+                int red = (rgb >> 16) & 0xFF;
+                int green = (rgb >> 8) & 0xFF;
+                int blue = rgb & 0xFF;
 
-                int newRgb;
-                if (component == 0) { // Red
-                    int red = (rgb >> 16) & 0xFF;
-                    red = (red & 0xFE) | bit;
-                    newRgb = (rgb & 0xFF00FFFF) | (red << 16);
-                } else if (component == 1) { // Green
-                    int green = (rgb >> 8) & 0xFF;
-                    green = (green & 0xFE) | bit;
-                    newRgb = (rgb & 0xFFFF00FF) | (green << 8);
-                } else { // Blue
-                    int blue = rgb & 0xFF;
-                    blue = (blue & 0xFE) | bit;
-                    newRgb = (rgb & 0xFFFFFF00) | blue;
+                for (int component = 0; component < 3; component++) {
+                    if (dataIndex >= data.length) break;
+
+                    int bit = (data[dataIndex] >> (7 - bitIndex % 8)) & 1;
+                    if (component == 0) {
+                        red = (red & 0xFE) | bit;
+                    } else if (component == 1) {
+                        green = (green & 0xFE) | bit;
+                    } else {
+                        blue = (blue & 0xFE) | bit;
+                    }
+
+                    bitIndex++;
+                    if (bitIndex % 8 == 0) {
+                        dataIndex++;
+                    }
                 }
 
+                int newRgb = (rgb & 0xFF000000) | (red << 16) | (green << 8) | blue;
                 image.setRGB(x, y, newRgb);
-
-                bitIndex++;
-                if (bitIndex % 8 == 0) {
-                    dataIndex++;
-                }
             }
         }
 
@@ -80,62 +121,63 @@ public class Steganography {
 
         int width = image.getWidth();
         int height = image.getHeight();
+        int totalPixels = width * height;
+        int totalBits = totalPixels * 3;
 
         // Extract length first
         byte[] lengthBytes = new byte[4];
         int bitIndex = 0;
-        int dataIndex = 0;
 
-        outer: for (int y = 0; y < height && dataIndex < 4; y++) {
-            for (int x = 0; x < width && dataIndex < 4; x++) {
-                int rgb = image.getRGB(x, y);
-                int component = bitIndex % 3;
-                int bit;
-
-                if (component == 0) { // Red
-                    bit = ((rgb >> 16) & 0xFF) & 1;
-                } else if (component == 1) { // Green
-                    bit = ((rgb >> 8) & 0xFF) & 1;
-                } else { // Blue
-                    bit = (rgb & 0xFF) & 1;
-                }
-
-                lengthBytes[dataIndex] |= (bit << (7 - bitIndex % 8));
-
-                bitIndex++;
-                if (bitIndex % 8 == 0) {
-                    dataIndex++;
-                }
+        for (; bitIndex < 32; bitIndex++) {
+            int pixelIndex = bitIndex / 3;
+            if (pixelIndex >= totalPixels) {
+                throw new IOException("Image does not contain enough data to read message length");
             }
+            int x = pixelIndex % width;
+            int y = pixelIndex / width;
+            int rgb = image.getRGB(x, y);
+            int component = bitIndex % 3;
+            int bit;
+
+            if (component == 0) {
+                bit = ((rgb >> 16) & 0xFF) & 1;
+            } else if (component == 1) {
+                bit = ((rgb >> 8) & 0xFF) & 1;
+            } else {
+                bit = (rgb & 0xFF) & 1;
+            }
+
+            lengthBytes[bitIndex / 8] |= (bit << (7 - bitIndex % 8));
         }
 
         int messageLength = ((lengthBytes[0] & 0xFF) << 24) | ((lengthBytes[1] & 0xFF) << 16) | ((lengthBytes[2] & 0xFF) << 8) | (lengthBytes[3] & 0xFF);
+        if (messageLength < 0) {
+            throw new IOException("Invalid hidden message length: " + messageLength);
+        }
 
-        // Extract message
+        int requiredBits = 32 + messageLength * 8;
+        if (requiredBits > totalBits) {
+            throw new IOException("Image does not contain enough data to read the hidden message");
+        }
+
         byte[] messageBytes = new byte[messageLength];
-        dataIndex = 0;
+        for (int i = 0; i < messageLength * 8; i++, bitIndex++) {
+            int pixelIndex = bitIndex / 3;
+            int x = pixelIndex % width;
+            int y = pixelIndex / width;
+            int rgb = image.getRGB(x, y);
+            int component = bitIndex % 3;
+            int bit;
 
-        outer2: for (int y = 0; y < height && dataIndex < messageLength; y++) {
-            for (int x = 0; x < width && dataIndex < messageLength; x++) {
-                int rgb = image.getRGB(x, y);
-                int component = bitIndex % 3;
-                int bit;
-
-                if (component == 0) { // Red
-                    bit = ((rgb >> 16) & 0xFF) & 1;
-                } else if (component == 1) { // Green
-                    bit = ((rgb >> 8) & 0xFF) & 1;
-                } else { // Blue
-                    bit = (rgb & 0xFF) & 1;
-                }
-
-                messageBytes[dataIndex] |= (bit << (7 - bitIndex % 8));
-
-                bitIndex++;
-                if (bitIndex % 8 == 0) {
-                    dataIndex++;
-                }
+            if (component == 0) {
+                bit = ((rgb >> 16) & 0xFF) & 1;
+            } else if (component == 1) {
+                bit = ((rgb >> 8) & 0xFF) & 1;
+            } else {
+                bit = (rgb & 0xFF) & 1;
             }
+
+            messageBytes[i / 8] |= (bit << (7 - i % 8));
         }
 
         return new String(messageBytes, "UTF-8");
